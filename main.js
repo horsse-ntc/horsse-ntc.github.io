de62afb27a6234017269a8d260842acdb69b5b60
horsse-ntc <162751164+horsse-ntc@users.noreply.github.com>
2024-03-09 21:00:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const tracker = document.getElementById('tracker')
+
+document.body.onpointermove = event => {
+    const {clientX, clientY} = event
+
+    tracker.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    }, {duration: 2500, fill: "forwards"})
+}
+
+
+const letters = "abcdefghijklmnopqrstuvwxyz";
+
+let interval = null;
+
+document.getElementById("title").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 0.25;
+  }, 20);
+}
